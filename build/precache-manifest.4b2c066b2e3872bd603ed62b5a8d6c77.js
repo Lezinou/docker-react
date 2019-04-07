9dc76180eb9c0ac2b13a3d2838ac93de4ba67fe2
Lezin <lezg@equinor.com>
2019-04-07 15:52:01 +0200

    Try to make it work

---
+++ build/precache-manifest.4b2c066b2e3872bd603ed62b5a8d6c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f564b6c04e5b2c7461ab",
+    "url": "/static/js/main.59fa1cc9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f564b6c04e5b2c7461ab",
+    "url": "/static/css/main.ae7b786f.chunk.css"
+  },
+  {
+    "revision": "90e31c737c341729cc4920598eeca08d",
+    "url": "/index.html"
+  }
+];
